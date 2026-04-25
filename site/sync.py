109221bac46011aea33250ce7071bc4dc1991351
Shevchenko Danil Mikhailovich <155669526+svyatoylol@users.noreply.github.com>
2026-04-25 17:01:06 +0500

    Update sync.py

--- site/sync.py
+++ site/sync.py
@@ -11,17 +11,17 @@ def run_cmd(cmd, cwd=None):
     try:
         subprocess.run(cmd, cwd=cwd, check=True, text=True, capture_output=True)
     except subprocess.CalledProcessError as e:
-        print(f"❌ Ошибка при выполнении: {' '.join(cmd)}")
+        print(f" Ошибка при выполнении: {' '.join(cmd)}")
         print(f"Подробности: {e.stderr.strip()}")
         sys.exit(1)
 
 def update_kb():
-    print(f"🔄 Синхронизация с базой знаний...")
+    print(f" Синхронизация с базой знаний...")
     
     os.makedirs(os.path.dirname(TARGET_DIR), exist_ok=True)
 
     if not os.path.exists(os.path.join(TARGET_DIR, ".git")):
-        print("📥 Первичная загрузка репозитория...")
+        print(" Первичная загрузка репозитория...")
         
         run_cmd(["git", "clone", "--filter=blob:none", "--no-checkout", REPO_URL, TARGET_DIR])
         
@@ -34,12 +34,12 @@ def update_kb():
             f.write("*.png\n*.jpg\n*.jpeg\n*.gif\n*.svg\n")
 
         run_cmd(["git", "checkout", BRANCH], cwd=TARGET_DIR)
-        print("✅ Первичная загрузка завершена!")
+        print(" Первичная загрузка завершена!")
         
     else:
-        print("📥 Подтягивание свежих изменений...")
+        print("Подтягивание свежих изменений...")
         run_cmd(["git", "pull", "origin", BRANCH], cwd=TARGET_DIR)
-        print("✅ Обновление успешно завершено!")
+        print(" Обновление успешно завершено!")
 
 if __name__ == "__main__":
     update_kb()
